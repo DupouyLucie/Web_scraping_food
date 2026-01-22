f5549f04fa2d6696c64894a5c5ac848da82d8678
Yannis AULLEN CHOUBRAC <you@example.com>
2026-01-22 09:42:52 +0100

    téléchargement database et graphique nutriscore

--- src/analyse.py
+++ src/analyse.py
@@ -1,18 +1,11 @@
 import sqlite3
 import pandas as pd
-from database import Database
 
 
-# db = Database("data/database.db")  # ouvre la base
-# List=db.get_all_produits() # On récupère toutes les infos
-# db.close()  # fermeture propre
+# Connexion et chargement
+conn = sqlite3.connect('data/database.db')
+df = pd.read_sql_query("SELECT * FROM produits", conn)
+conn.close()
 
-df = pd.read_sql_query('''SELECT * FROM produit''', con=sqlite3)
-
-
-
-#analyse 1 
-# for el in 
-
-if __name__ == "__main__":
-    print(df.head(10))
+# On transforme la colonne ingredients en une liste
+df['ingredients'] = df['ingredients'].str.split(',')
